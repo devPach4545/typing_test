83f889ad1e559da6922b90f20303a47bba21a092
devPach4545 <dhaivatpachchigar@gmail.com>
2023-09-05 22:10:07 -0700

    shows stats and ends in 30 seconds

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import *
 import time
 import random
 from tkinter import messagebox
+
 window = Tk()
 window.geometry("1000x800")
 window.title("Dev's typing test")
@@ -11,51 +12,41 @@ data_loaded = False
 data = ""
 
 def get_data():
-    global text_data,start_time
+    global text_data, start_time
     lines = open('data.txt').read().splitlines()
     for _ in range(10):
-        myline = random.choice(lines)    
+        myline = random.choice(lines)
     text_data = Text()
-    text_data.insert("1.0",myline)
-    text_data.config(height=8, width=100,font=("Arial",15,"bold"),background="red")
-    text_data.place(relx=0.5,rely=0.2,anchor=CENTER)
+    text_data.insert("1.0", myline)
+    text_data.config(height=8, width=100, font=("Arial", 15, "bold"), background="red")
+    text_data.place(relx=0.5, rely=0.2, anchor=CENTER)
     start_time = time.time()
-    
-   
-    
-
 
 def reset_data():
-   text_data.delete("1.0",END)
-   text_input_area.delete("1.0",END)
-   update_speed_labels(0, 0)
+    text_data.delete("1.0", END)
+    text_input_area.delete("1.0", END)
+    update_speed_labels(0, 0)
 
-#addint the text input area
-text_input_area = Text(height=10,width=100,font=("Helvetica",15,"bold"),background="orange")
-text_input_area.place(relx=0.5,rely=0.5,anchor=CENTER)
+text_input_area = Text(height=10, width=100, font=("Helvetica", 15, "bold"), background="orange")
+text_input_area.place(relx=0.5, rely=0.5, anchor=CENTER)
 
 start_image = PhotoImage(file="start_button.png")
 start_button = Button(image=start_image, highlightthickness=0, command=get_data)
 start_button.place(relx=0.2, rely=0.8, anchor=CENTER)
 
-
 reset_image = PhotoImage(file="reset.png")
-reset_button = Button(image=reset_image,highlightthickness=0,command=reset_data)
-reset_button.place(relx=0.8,rely=0.8,anchor=CENTER)
-
+reset_button = Button(image=reset_image, highlightthickness=0, command=reset_data)
+reset_button.place(relx=0.8, rely=0.8, anchor=CENTER)
 
 def update_speed_labels(cpm, wpm):
-    speed_label.config(text=f"Speed: \n{cpm:.2f} CPM\n{wpm:.2f} WPM")    
-
+    speed_label.config(text=f"Speed: \n{cpm:.2f} CPM\n{wpm:.2f} WPM")
 
 def compare_text(event):
     user_input = text_input_area.get("1.0", "end-1c")
     original_text = text_data.get("1.0", "end-1c")
 
-    # Remove any previous highlighting
     text_input_area.tag_remove("highlight", "1.0", "end")
 
-    # Compare user input and original text character by character
     global correct_words
     correct_words = 0
     for i, (user_char, original_char) in enumerate(zip(user_input, original_text)):
@@ -69,33 +60,29 @@ def compare_text(event):
 
     elapsed_time = time.time() - start_time
 
-    # Calculate CPM and WPM based on correct words
-    cpm = (correct_words * 5) / elapsed_time * 60  # Assuming an average word length of 5 characters
-    wpm = cpm / 5  # Assuming an average word length of 5 characters
+    cpm = (correct_words * 5) / elapsed_time * 60
+    wpm = cpm / 5
     update_speed_labels(cpm, wpm)
 
 def show_finish_stats():
     global start_time
     elapsed_time = time.time() - start_time
-    cpm = (correct_words * 5) / elapsed_time * 60  # Assuming an average word length of 5 characters
-    wpm = cpm / 5  # Assuming an average word length of 5 characters
+    cpm = (correct_words * 5) / elapsed_time * 60
+    wpm = cpm / 5
 
     finish_message = f"Your final stats:\n\nCPM: {cpm:.2f}\nWPM: {wpm:.2f}"
     messagebox.showinfo("Typing Test Finish", finish_message)
-
-# ... (Your existing code)
+    window.quit()  # Close the window after displaying the result
 
 finish_button = Button(text="Finish", font=("Arial", 15, "bold"), command=show_finish_stats)
 finish_button.place(relx=0.5, rely=0.9, anchor=CENTER)
-   
-
-    
 
 text_input_area.bind("<Key>", compare_text)
 
 speed_label = Label(text="Speed: \n0.00 CPS\n WPM")
-speed_label.place(relx= 0.5, rely=0.8,anchor=CENTER)
+speed_label.place(relx=0.5, rely=0.8, anchor=CENTER)
 
-
+# Schedule the function to end the test and show the result after 30 seconds
+window.after(30000, show_finish_stats)
 
 window.mainloop()
